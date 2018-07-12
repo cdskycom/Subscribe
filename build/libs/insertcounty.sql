cb84ba047036355b850bbab666a8b01790e12d41
谢卫忠 <cdskycom@163.com>
2018-07-12 23:52:12 +0800

    add insert county

---
+++ build/libs/insertcounty.sql
@@ -0,0 +1,18 @@
+/*
+-- Query: SELECT * FROM subscribenew.county
+LIMIT 0, 400
+
+-- Date: 2018-07-12 23:49
+*/
+INSERT INTO `county` (`id`,`name`,`leader_id`,`type`) VALUES (1,'宜宾',NULL,'CTY');
+INSERT INTO `county` (`id`,`name`,`leader_id`,`type`) VALUES (2,'屏山县',1,NULL);
+INSERT INTO `county` (`id`,`name`,`leader_id`,`type`) VALUES (3,'兴文县',1,NULL);
+INSERT INTO `county` (`id`,`name`,`leader_id`,`type`) VALUES (4,'筠连县',1,NULL);
+INSERT INTO `county` (`id`,`name`,`leader_id`,`type`) VALUES (5,'高县',1,NULL);
+INSERT INTO `county` (`id`,`name`,`leader_id`,`type`) VALUES (6,'珙县',1,NULL);
+INSERT INTO `county` (`id`,`name`,`leader_id`,`type`) VALUES (7,'长宁县',1,NULL);
+INSERT INTO `county` (`id`,`name`,`leader_id`,`type`) VALUES (8,'江安县',1,NULL);
+INSERT INTO `county` (`id`,`name`,`leader_id`,`type`) VALUES (9,'宜宾县',1,NULL);
+INSERT INTO `county` (`id`,`name`,`leader_id`,`type`) VALUES (10,'翠屏区',1,NULL);
+INSERT INTO `county` (`id`,`name`,`leader_id`,`type`) VALUES (11,'临港区',1,NULL);
+INSERT INTO `county` (`id`,`name`,`leader_id`,`type`) VALUES (12,'南溪区',1,NULL);
